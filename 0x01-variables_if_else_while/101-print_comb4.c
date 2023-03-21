770e7d20bcbf2db88405ce16a72912ea80f30a74
sayed <sayed@gmail.com>
2023-03-20 18:20:54 -0700

    Hot add

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ *main - Prints the numbers from 00 to 99, numbers separated by
+ *       a comma followed by a space, in ascending order.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			for (num3 = 0; num3 < 10; num3++)
+			{
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
+				if (num1 == 9 && num2 == 9 && num3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
