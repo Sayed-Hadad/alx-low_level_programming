80d22ee69852fa82320ffd92547a526bb6054932
sayed <sayed@gmail.com>
2023-03-23 04:52:14 -0700

    s

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * 
+ *function that checks for uppercase character
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
