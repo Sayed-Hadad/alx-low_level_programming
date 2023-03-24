c25f003c42e1503895eb0001d1a9fea1c0cd6642
sayed <sayed@gmail.com>
2023-03-24 16:27:01 -0700

    d

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -7,9 +7,13 @@
  */
 int main(void)
 {
+	int i;
+
 	print_diagonal(0);
 	print_diagonal(2);
 	print_diagonal(10);
 	print_diagonal(-4);
+	for(i = 1 ; i <= 10 ; i++)
+		print_diagonal(i);
 	return (0);
 }
