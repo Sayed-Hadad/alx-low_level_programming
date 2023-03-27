1d72a0489f06f730446c814fa03be8ffb15ac374
sayed <sayed@gmail.com>
2023-03-27 16:10:05 -0700

    s

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_rev - prints a string, in reverse
+ *
+ *@s : input
+ *Return : void
+ */
+
+void print_rev(char *s)
+{
+        int counter, i;
+
+        while (s[counter] != '\0')
+        {
+                counter++;
+        }
+
+        for (i = counter - 1; i >= 0; i--)
+        {
+                _putchar(s[i]);
+        }
+        _putchar('\n');
+}
