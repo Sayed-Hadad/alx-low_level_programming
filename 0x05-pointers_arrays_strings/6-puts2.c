5224de71c761a18e3c6263da280d1059da56c4a7
sayed <sayed@gmail.com>
2023-03-27 19:47:03 -0700

    ss

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
 /**
- *puts2 - prints every char in string
- *
- *@str: the input
+ * puts2 - print the 
+ * followed by a new line
+ * @str: input
  */
 void puts2(char *str)
 {
-	int counter = 0, i;
+	int counter, i;
+
+	counter = 0;
 
 	while (str[counter] != '\0')
 	{
 		counter++;
 	}
 
-	for (i = 0; i <= counter; i += 2)
+	for (i = 0; i < counter; i += 2)
 	{
 		_putchar(str[i]);
 	}
+
 	_putchar('\n');
 }
