4d76ae3ccc42708ea817144c3c9ce64baf0a1b89
sayed <sayed@gmail.com>
2023-03-30 18:51:09 -0700

    s

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *string_toupper - convert string to upper
+ *@str : input
+ *Return: str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] -= 32;
+		}
+	}
+
+	return str;
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *cap_string(char *);
 int _putchar(char);
+char *string_toupper(char *);
