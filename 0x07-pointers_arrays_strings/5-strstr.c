9719cc29cdd1530a5b4be9fe6dd8c8addd2f9455
sayed <sayed@gmail.com>
2023-04-04 19:02:17 -0700

    s

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,7 +7,7 @@
  */
 char *_strstr(char *haystack, char *needle){
     int i = 0;
-    while(haystack[i] != "\0")
+    while(haystack[i] != '\0')
     {
             if(needle[i] == haystack[i])
             {
