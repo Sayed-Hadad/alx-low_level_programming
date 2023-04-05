a9b7e53f86ad7400368073136c50fcce1f4e2e17
sayed <sayed@gmail.com>
2023-04-04 18:38:21 -0700

    s

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strstr -  function that locates a substring.
+ *@haystack : the source str.
+ *@needle : the search element.
+ *Return: the result of search
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	while (*(haystack + i) != '\0')
+	{
+		if (*(needle + i) == *(haystack + i))
+		{
+			return (needle);
+		}
+
+		i++;
+	}
+
+	return ('\0');
+}
