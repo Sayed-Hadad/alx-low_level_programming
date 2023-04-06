88add3ab578fc590a18f4d40900413cebb73dbd6
sayed <sayed@gmail.com>
2023-04-06 13:11:02 -0700

    s

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,11 @@
  *
  *Return: The length of the string.
  */
-int _strlen_recursion(char *s)
+int _strlen_recursion(char * s)
 {
 	int longit = 0;
-	if (*s)
+
+	if (* s)
 	{
 		longit++;
 		longit += _strlen_recursion(s + 1);
