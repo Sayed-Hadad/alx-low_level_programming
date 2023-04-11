f1efccfa6ccc9d70210a180d309c718317567421
sayed <sayed@gmail.com>
2023-04-10 19:11:54 -0700

    First Done!

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h >
+
+/**
+ *main - print the name of program
+ *@argc : the counter of arguments
+ *@argv : the array of strings contain the arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
