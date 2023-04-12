cfa935e8fc1cf7724d4c76126454e898ba07e762
sayed <sayed@gmail.com>
2023-04-11 20:54:16 -0700

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,22 +9,22 @@
  */
 int main(int argc, char *argv[])
 {
-        int i, mult = 1;
+	int i, mult = 1;
 
-        if (argc == 1 || argc == 2)
-        {
-                printf("Error\n");
-                return (1);
-        }
-        else
-        {
-                for (i = 1; i < 3; i++)
-                {
-                        mult *= atoi(argv[i]);
-                }
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < 3; i++)
+		{
+			mult *= atoi(argv[i]);
+		}
 
-                printf("%d\n", mult);
-        }
+		printf("%d\n", mult);
+	}
 
-        return (0);
+	return (0);
 }
