90cfee120998348aa68a46a2d2fb840bcceb9ac4
sayed <sayed@gmail.com>
2023-04-18 17:12:55 -0700

    s

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,19 +2,29 @@
 #define _HEADER_
 
 /**
-  * struct dog - Dog attributes
-  * @name: The name of the dog.
-  * @age: The age of the dog.
-  * @owner: The owner of the dog.
-  *
-  * Description: The attributes of a dog.
-  */
+ *struct dog - Dog attributes
+ *@name: The name of the dog.
+ *@age: The age of the dog.
+ *@owner: The owner of the dog.
+ *
+ *Description: The attributes of a dog.
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+dog_t;
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
