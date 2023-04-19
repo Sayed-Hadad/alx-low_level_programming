5804b1119ff82c131e3912005f2a684455291612
sayed elhadad <anasesozaking@gmail.com>
2023-04-20 00:36:56 +0200

    s

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,21 +1,27 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
-
+#include <stdarg.h>
+#include <stdio.h>
 /**
- *sum_them_all - function to sum infinity nums
- *@n : number of nums
- *return : sum
+ * sum_them_all - The entery point
+ * @n: inputs
+ * Return: sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0, i;
+	va_list valist;
+	unsigned int i, sum = 0;
 
-	va_list ptr;
-	va_start(ptr, n);
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
+
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(ptr, int);
+		sum += va_arg(valist, const unsigned int);
 	}
 
+	va_end(valist);
+
 	return (sum);
-};
+}
