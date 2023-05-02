5196fbcff5f131660d2f3cecc1a6ee0cfbde5cd3
sayed <sayed@gmail.com>
2023-05-02 12:01:36 -0700

    Betty Fix!

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@
  *Return: If the function fails - NULL.
  *        Otherwise - the address of the new element.
  */
-listint_t* add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new, *last;
 
