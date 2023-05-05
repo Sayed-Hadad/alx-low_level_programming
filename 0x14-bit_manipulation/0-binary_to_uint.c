ac2cbebc9db6ffe07ce90b6ceae9c1704da1cb84
sayed <sayed@gmail.com>
2023-05-05 12:57:06 -0700

    s

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,25 +7,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int k = 1;
-	unsigned int i = 0;
-	int c;
-	unsigned int len;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
-	len = strlen(b);
-	if(b == NULL)
+	if (b == NULL)
 		return (0);
-	for (c = len - 1; c >= 0; c--)
-	{
-		if (b[c] != '0' && b[c] != '1')
-			return (0);
-		if (b[c] == '1')
-		{
-			i += k;
-		}
 
-		k *= 2;
+	while (b[i + 1])
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
 	}
 
-	return (i);
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
 }
